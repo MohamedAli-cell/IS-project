923830d99240186d3154ce53fab77b1f0e091566
mohamed ali <mohamedali1139@gmail.com>
2019-10-11 19:17:35 +0200

    first commit

---
+++ trying/trying/trying.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	int width,lenth;
+
+
+		
+	cout<<"please enter the width \n";
+	cin>>width;
+
+	cout<<"please enter the lenth" <<"\n";
+	cin>>lenth;
+
+	
+	cout<<"the area = \n"<<width*lenth <<"\n";
+	system("pause");
+
+
+	return 0;
+
+}
